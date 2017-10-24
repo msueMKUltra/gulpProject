e8bd704c03547de1c81a6eff018abd36c0dbe5c7
Roller Lo <jasonbh02@gmail.com>
2017-10-24 15:50:26 +0800

    Update 2017-10-24T07:50:24.605Z

--- js/all.js
+++ js/all.js
@@ -1,13 +1,2 @@
-'use strict';
-
-var newFn = function newFn() {
-	console.log('aaa');
-};
-newFn();
-'use strict';
-
-var newFn2 = function newFn2() {
-	console.log('ccc');
-};
-newFn2();
+"use strict";var newFn=function(){};newFn();var newFn2=function(){};newFn2();
 //# sourceMappingURL=all.js.map
