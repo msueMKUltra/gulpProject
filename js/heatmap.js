d0b610ed3015ba085794916ecafd0c182e528eba
Roller Lo <jasonbh02@gmail.com>
2018-05-27 16:04:28 +0800

    Update 2018-05-27T08:04:22.362Z

---
+++ js/heatmap.js
@@ -0,0 +1,2 @@
+"use strict";$(function(){var a={container:document.getElementById("heatmapContainer"),radius:10,maxOpacity:.5,minOpacity:0,blur:.75},t=h337.create(a),e={x:50,y:50,value:5};t.addData(e);e={x:60,y:60,value:5};t.addData(e)});
+//# sourceMappingURL=heatmap.js.map
